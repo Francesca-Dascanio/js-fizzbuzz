bf0ab9d669829d33b9e1688bbdfba1b937418928
“Francesca <dascanio.francesca@gmail.com>
2022-12-21 16:56:05 +0100

    Inserita istruzione condizionale a numeri multipli di 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
+let message;
+
 //Stampa numeri da 1 a 100
 for (let n=1; n <= 100; n = n + 1) {
-    console.log(n);
+    message = n;
     
+
+    // Se il numero diviso 3 fa zero (quindi è 3 o multiplo di 3) allora quel numero compare come testo Fizz
     if (n % 3 == 0) {
-        console.log('Fizz');
+        message = 'Fizz';
     }
+    console.log(message);
 
 
 }
